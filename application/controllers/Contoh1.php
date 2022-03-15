83911fe65df9fd86bc4fd985ad4ba2adac508a72
Agung19207226 <19207226@bsi.ac.id>
2022-03-16 00:07:10 +0700

    Instalasi CI dan Contoh1 application,system,.editorconfig,.gitinore,composer.json,index.php,license.txt,readme.rst

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhamad Agung Samudra
+            Saya tinggal di daerah Depok - Jawa Barat
+            Olahraga yang saya sukai adalah Basket";
+    }
