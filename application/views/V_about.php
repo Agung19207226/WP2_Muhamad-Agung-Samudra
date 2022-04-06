1836f87952c98f2f6703ef7f13dcfc631b5651b0
Agung19207226 <19207226@bsi.ac.id>
2022-04-06 20:32:48 +0700

    commit

---
+++ application/views/V_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhamad Agung</li>
+        <li>Nama Belakang : Samudra</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Pipit No.2 Rt.001/002 Kel.Beji Timur Kec. Beji Kota Depok</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Depok</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Joging</li>
+    </ul>
+</section>
